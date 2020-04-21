213c7f21bb237c0ee3192d748dbead26eba4409d
giulio <giulio09@marcov.it>
2020-04-21 19:31:06 +0000

    Updates

---
+++ precache-manifest.2eb716283531538363f8c279e6ee16c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6e3536bed0a99a6437de33c5366440",
+    "url": "/scherma/index.html"
+  },
+  {
+    "revision": "9f135dae18a89ed61512",
+    "url": "/scherma/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7aa7ebb06a326b473d15",
+    "url": "/scherma/static/js/2.f98499d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scherma/static/js/2.f98499d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f135dae18a89ed61512",
+    "url": "/scherma/static/js/main.50531142.chunk.js"
+  },
+  {
+    "revision": "34b204352d4a2a3551f1",
+    "url": "/scherma/static/js/runtime-main.a2ef1780.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scherma/static/media/logo.5d5d9eef.svg"
+  }
+]);
