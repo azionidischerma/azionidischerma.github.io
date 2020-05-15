e527c0bbc85f0f3e7ccb080848cd258795833d65
giulio <giulio09@marcov.it>
2020-05-15 11:32:21 +0000

    Updates

---
+++ precache-manifest.2eea4f886f9576db41169cba2b3144b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8fd5cfb8511998d74aea89d0ede693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50d3b8a7c94ddaa1c8d8",
+    "url": "/static/css/main.3a48a1e1.chunk.css"
+  },
+  {
+    "revision": "432817f62cafaaa11a38",
+    "url": "/static/js/2.920b5cd4.chunk.js"
+  },
+  {
+    "revision": "9b106367bfdd2f660fb9d31b8b1921c8",
+    "url": "/static/js/2.920b5cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d3b8a7c94ddaa1c8d8",
+    "url": "/static/js/main.c08cb88c.chunk.js"
+  },
+  {
+    "revision": "89cca70264b1d0cf02e1",
+    "url": "/static/js/runtime-main.57a06b3c.js"
+  }
+]);
