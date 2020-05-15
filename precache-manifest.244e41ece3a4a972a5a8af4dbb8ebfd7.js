38e828cf439cbdf931faef6a61cb2cabb1bf66f6
giulio <giulio09@marcov.it>
2020-05-15 11:13:42 +0000

    Updates

--- precache-manifest.244e41ece3a4a972a5a8af4dbb8ebfd7.js
+++ precache-manifest.244e41ece3a4a972a5a8af4dbb8ebfd7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "505602bb526620933ac297fb486e6aa4",
+    "revision": "3c52c7b841e8e5553bac931da740333c",
     "url": "/scherma/index.html"
   },
   {
-    "revision": "ba5732bd6921509a7a48",
+    "revision": "28beae303a05d730555d",
     "url": "/scherma/static/css/main.3a48a1e1.chunk.css"
   },
   {
-    "revision": "432817f62cafaaa11a38",
-    "url": "/scherma/static/js/2.920b5cd4.chunk.js"
+    "revision": "67419e1e745819e78d46",
+    "url": "/scherma/static/js/2.23abb749.chunk.js"
   },
   {
     "revision": "9b106367bfdd2f660fb9d31b8b1921c8",
-    "url": "/scherma/static/js/2.920b5cd4.chunk.js.LICENSE.txt"
+    "url": "/scherma/static/js/2.23abb749.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ba5732bd6921509a7a48",
-    "url": "/scherma/static/js/main.5056c2d9.chunk.js"
+    "revision": "28beae303a05d730555d",
+    "url": "/scherma/static/js/main.a3ce09d5.chunk.js"
   },
   {
     "revision": "34b204352d4a2a3551f1",
